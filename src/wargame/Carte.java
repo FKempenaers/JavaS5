197008fdac80187f46e8b5425770fc1fd8b2df79
Apache <Apache@Apache-PC>
2017-11-11 13:59:24 +0100

    correction TrouvePositionVide()

--- src/wargame/Carte.java
+++ src/wargame/Carte.java
@@ -3,13 +3,13 @@ package wargame;
 import java.awt.Graphics;
 import java.util.Random;
 
-public class Carte implements ICarte, IConfig{
+public class Carte implements ICarte, IConfig {
 	private Element[][] carte;
-	
+
 	public Carte() {
-	this.carte = new Element[LARGEUR_CARTE][HAUTEUR_CARTE];
+		this.carte = new Element[LARGEUR_CARTE][HAUTEUR_CARTE];
 	}
-	
+
 	@Override
 	public Element getElement(Position pos) {
 		return carte[pos.getX()][pos.getY()];
@@ -17,16 +17,16 @@ public class Carte implements ICarte, IConfig{
 
 	@Override
 	public Position trouvePositionVide() {
-		int x,y;
+		int x, y;
 		x = y = 0;
 		Random r = new Random();
-		Element e = null;
-		while(e == null) {
+		Element e;
+		do {
 			x = r.nextInt(LARGEUR_CARTE);
 			y = r.nextInt(HAUTEUR_CARTE);
-			e = carte[x][y];		
-		}
-		return new Position(x,y);
+			e = carte[x][y];
+		} while (e != null);
+		return new Position(x, y);
 	}
 
 	@Override
@@ -56,7 +56,7 @@ public class Carte implements ICarte, IConfig{
 	@Override
 	public void mort(Soldat perso) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -68,13 +68,13 @@ public class Carte implements ICarte, IConfig{
 	@Override
 	public void jouerSoldats(PanneauJeu pj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void toutDessiner(Graphics g) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
